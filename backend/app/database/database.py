c9d8c315cf414f67694b2c77a9a25c923283653f
daniilvagin <daniil.vg@outlook.com>
2025-09-10 15:29:42 +0300

    Added session management and configuration

---
+++ backend/app/database/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+from core import *
+
+engine = create_engine(
+    settings.database_url,
+    echo=settings.database_echo
+)
+
+SessionLocal: sessionmaker[Session] = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
