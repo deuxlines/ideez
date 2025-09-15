fca35afd686128e0523edc749527ea7e393cd944
daniilvagin <daniil.vg@outlook.com>
2025-09-15 14:03:09 +0300

    Refactor for better ID storing

--- backend/app/database/base.py
+++ backend/app/database/base.py
@@ -1,5 +1,12 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, 
+from sqlalchemy.dialects.postgresql import UUID
+
+import uuid
 
 
 class Base(DeclarativeBase):
-    pass
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), 
+        primary_key=True, 
+        default=uuid.uuid4
+    )
