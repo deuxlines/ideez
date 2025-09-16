fb06ce2110c022896716c8d4302028c1022e0b49
daniilvagin <daniil.vg@outlook.com>
2025-09-16 13:50:36 +0300

    Refactor the init file

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,3 +1,5 @@
 from .core import settings
-from .api.v1 import *
+from .api.v1 import auth_router
 from .infrastructure.application import create_app
+
+__all__ = ["settings", "auth_router", "create_app",]
