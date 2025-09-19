8a00fb4c6470dd00accad478848af93e96cde22a
daniilvagin <daniil.vg@outlook.com>
2025-09-19 15:13:36 +0300

    create main FastAPI app entrypoint

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import uvicorn
+
+from app import video_router, auth_router, settings, create_app, setup_middleware
+
+app: FastAPI = create_app(
+    title = settings.app_name,
+    debug = settings.debug,
+    use_sessions = True,
+    routers = [
+        auth_router,
+        video_router
+    ]
+)
+
+setup_middleware(app)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
