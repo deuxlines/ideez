fcebc2a9f4c2aab2ab4ecd43873e476c73b44286
daniilvagin <daniil.vg@outlook.com>
2025-09-25 15:27:13 +0300

    Change auth flow from server-side

--- backend/app/api/v1/authorization.py
+++ backend/app/api/v1/authorization.py
@@ -1,7 +1,9 @@
-from fastapi import Request, APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from authlib.integrations.starlette_client import OAuth
+from google.oauth2 import id_token
+from google.auth.transport import requests as google_requests
 
+from app.schemas import GoogleToken
 from app.core import settings
 from app.auth import create_access_token
 from app.database import get_db
@@ -10,31 +12,21 @@ from app.crud import create_user, get_user
 
 auth_router: APIRouter = APIRouter(prefix="/auth", tags=["Auth"])
 
-oauth = OAuth()
-oauth.register(
-    name="google",
-    client_id=settings.client_id,
-    client_secret=settings.client_secret,
-    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
-    client_kwargs={"scope": "openid email profile"},
-)
-
-@auth_router.get("/login")
-async def login(request: Request):
-    redirect_uri = request.url_for("auth_callback")
-    return await oauth.google.authorize_redirect(request, redirect_uri)
-
-@auth_router.get("/callback", name="auth_callback")
-async def auth_callback(request: Request, db: Session = Depends(get_db)):
-    token = await oauth.google.authorize_access_token(request)
-    user_info: dict = await oauth.google.parse_id_token(token, nonce=None)
-    user: User | None = get_user(db, user_info["sub"])
-    if not user:
-        user = create_user(
-            db=db,
-            user_info=user_info
+@auth_router.post("/google")
+async def google_auth(data: GoogleToken, db: Session = Depends(get_db)):
+    try:
+        idinfo = id_token.verify_oauth2_token(
+            data.token, google_requests.Request(),
+            settings.client_id
         )
-    
-    access_token = create_access_token(data={"sub": str(user.id)})
-
-    return {"access_token": access_token, "token_type": "bearer"}
+        # print(idinfo)
+        user: User | None = get_user(db, idinfo["sub"])
+        if not user:
+            user = create_user(
+                db=db,
+                user_info=idinfo
+            )
+        return {"access_token": create_access_token(data={"sub": str(user.id)}), "user": {"email": idinfo["email"], "name": idinfo.get("name"), "picture": idinfo["picture"]}}
+    except ValueError as e:
+        # print(e)
+        raise HTTPException(status_code=400, detail="Invalid Google token")
