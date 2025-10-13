3b0966aa99eb3d29c162288d75861601db3ea05a
daniilvagin <daniil.vg@outlook.com>
2025-10-13 12:03:41 +0300

    add login and register auth schemas

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,5 +1,14 @@
 from .user_schemas import UserCreate, UserResponse, UserUpdate
 from .video_schemas import VideoCreate, VideoRead
-from .auth_schemas import GoogleToken
+from .auth_schemas import GoogleToken, LoginRequest, RegisterRequest
 
-__all__ = ["UserCreate", "UserResponse", "UserUpdate", "VideoRead", "VideoCreate", "GoogleToken",]
+__all__ = [
+    "UserCreate", 
+    "UserResponse", 
+    "UserUpdate", 
+    "VideoRead", 
+    "VideoCreate", 
+    "GoogleToken", 
+    "LoginRequest", 
+    "RegisterRequest"
+]
--- backend/app/schemas/auth_schemas.py
+++ backend/app/schemas/auth_schemas.py
@@ -1,4 +1,22 @@
-from pydantic import BaseModel
+from typing import Annotated
+
+from pydantic import BaseModel, EmailStr, constr
+
+
+PasswordStr = Annotated[str, constr(strip_whitespace=True, min_length=8)]
+NameStr = Annotated[str, constr(strip_whitespace=True, min_length=1)]
+
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: PasswordStr
+
+
+class RegisterRequest(BaseModel):
+    name: NameStr
+    email: EmailStr
+    password: PasswordStr
+
 
 class GoogleToken(BaseModel):
     token: str
