19df95550d17501a03f5076af3b3e537ec6e977e
daniilvagin <daniil.vg@outlook.com>
2025-10-13 12:04:27 +0300

    add several get user methods for different auth flows

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -1,10 +1,13 @@
-from .user import get_user_by_google_id, create_user, get_current_user
+from .user import get_user_by_google_id, create_google_user, create_password_user, get_current_user, get_user_by_email
 from .video import get_random_video, create_video, get_all_videos
 
-__all__ = ["create_user", 
-           "get_user_by_google_id", 
-           "get_random_video", 
-           "create_video", 
-           "get_all_videos", 
-           "get_current_user",
-        ]
+__all__ = [
+   "create_google_user", 
+   "create_password_user",
+   "get_user_by_google_id", 
+   "get_random_video", 
+   "create_video", 
+   "get_all_videos", 
+   "get_current_user",
+   "get_user_by_email",
+]
--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -1,11 +1,19 @@
 from uuid import UUID
 
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 
 from app.domain import User
+from app.auth import hash_password
 
 def get_current_user(db: Session, id: UUID) -> User | None:
     user = db.query(User).filter(User.id == id).first()
+    if not user:
+        return None
+    return user
+
+def get_user_by_email(db: Session, email: str) -> User | None:
+    user = db.query(User).filter(User.email == email).first()
     if not user:
         return None
     return user
@@ -16,13 +24,24 @@ def get_user_by_google_id(db: Session, google_id: str) -> User | None:
         return None
     return user
 
-def create_user(db: Session, user_info: dict) -> User:
+def create_google_user(db: Session, user_info: dict) -> User:
     user = User()
-    user.name = user_info.get("name", "No Name")
     user.google_id = user_info["sub"]
+    user.name = user_info.get("name", "No Name")
     user.email = user_info["email"]
     user.picture_url=user_info.get("picture", "")
 
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+def create_password_user(db: Session, user_info: dict) -> User:
+    user = User()
+    user.hashed_password = hash_password(user_info["password"])
+    user.name = user_info.get("name", "No Name")
+    user.email = user_info["email"]
+    
     db.add(user)
     db.commit()
     db.refresh(user)
